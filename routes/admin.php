b49d1fffbdfa7e14fff8af0e6584bb2829e66ebc
Umer Farooq <umerfarooq.697@gmail.com>
2022-04-07 13:39:45 +0500

    FIRST COMMIT

---
+++ routes/admin.php
@@ -0,0 +1,5 @@
+<?php
+
+
+Route::view('/backpanel', 'backpanel.dashboard.index');
+
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your application. These
+| Here is where you can register web` routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/add-roles', function(){
+    $roles = [
+        ['name' => 'admin', 'guard_name' => 'web'],
+        ['name' => 'author', 'guard_name' => 'web'],
+        ['name' => 'editor', 'guard_name' => 'web'],
+    ];
+    $role = Role::insert($roles);
+    return 'success';
+});
+
+
+require('admin.php');
